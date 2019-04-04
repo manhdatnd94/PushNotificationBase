bceb6a4350b57ec248b35ffbbeef711c241a14a7
DatPM <DatPM@fsoft.com.vn>
2019-04-04 22:08:01 +0700

    init commit

---
+++ NotifcationSample/NotifcationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotifcationSample
+//
+//  Created by Nguyễn Nam on 3/6/19.
+//  Copyright © 2019 Hypertech Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
